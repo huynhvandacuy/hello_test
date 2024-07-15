707122d4a6145e3dfd2623fb20ddb75ceb405515
TommyHuynh <dacuy.huynh6905@gmail.com>
2024-07-15 20:24:56 +0700

    frist cpp code

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout <<"Hello world";
+    cout<<"\nThe King is back";
+    for(int i = 0; i < 2; i++) {
+        cout<<"\nChu ba dep trai";
+
+    }
+    return 0;
+}
